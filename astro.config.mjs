dfcca76dec4a22d1e83f6d2f29d722f5c1975b2d
trinitybaz <trinityjbaz@gmail.com>
2026-08-29 21:53:46 -0400

    unit 6 done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
-  site: 'https://trinitybaz.github.io'
+  site: 'https://trinitybaz.github.io',
+  integrations: [preact()]
 });
